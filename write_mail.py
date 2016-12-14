b600432afa24ed9a658fa95674a32cef45828113
MALC <talhacelik81@hotmail.com>
2016-12-14 20:20:53 +0200

    Add files via upload

--- write_mail.py
+++ write_mail.py
@@ -1,25 +1,55 @@
 #!/usr/bin/env python
 
 import smtplib
+import time
+
 class MailWriter (object):
-    """for send to reminders"""
+    '''
+    Mail sender Class
+    '''
     def __init__(self):
         print("Mail Sender Class")
 
-    def al(self, word):
-        return input(self.word).strip()
+    def write(self , to):
 
-    def write(self , client):
+        '''
+        :param to: Where to send mail. ex: my_mail@server.com
+        :return: void
+        '''
+
+        __server = 'smtp.gmail.com'
+        __from = 'reminder@malc.com'
+        __to = to
+        __subject = "SUBJECT"
 
         try:
-            #smtplib.SMTP('smtp.google.com', 587)
+            __mail=smtplib.SMTP(__server, 587)
 
-            _mail = smtplib.SMTP_SSL('smtp.google.com',587)
-            _mail.sendmail('reminder@malc.com', client, "konu")
+            #__mail = smtplib.SMTP_SSL(__server,587)
+            __mail.ehlo()
+            __mail.starttls()
+            __mail.login('malc.reminder@gmail.com', 'karaman70')
+            __mail.sendmail(__from, __to, __subject)
 
-            _mail.quit()
         except Exception as e:
             print("Error Info : "+str(e))
+        finally:
+            __mail.close()
+            print ("Mail Send. To %s\n" %(__to))
+
+    def timer(self, to, time):
+        '''
+        :param to: mail from to send
+        :param time: Given to hour type
+        :return: void
+        '''
+
+        print (to)
+        print (time)
+        self.write(to)
+
+
+
 
 
 """
